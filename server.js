6b7f88e58a170ef099f8b9127bd2c0c3e798cdb3
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-03-28 14:19:11 +0200

    basis SW wrokt

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ import fetch from './routes/fetchdata.mjs';
 import {API_KEY , API_URL} from './routes/fetchdata.mjs';
 
 const app = express();
-const __dirname = path.resolve();
+// Configureer de app om JSON-gegevens te kunnen ontvangen en interpreteren
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
-app.set('views', 'views')
-app.set('view engine', 'ejs');
-// app.use('/', express.static(__dirname + '/'));
 
+// Configureer de app om URL-gecodeerde gegevens te kunnen ontvangen en interpreteren
+app.use(express.urlencoded({extended: true}));
+
+// Configureer de app om statische bestanden te kunnen serveren vanuit de 'public' map
+app.use(express.static("public"));
+
+// Configureer de 'views' map als de directory voor alle weergavebestanden (views)
+app.set('views', 'views')
+
+// Configureer de 'ejs' module als de view engine voor de app
+app.set('view engine', 'ejs');
 
 app.use('/', router);
 
