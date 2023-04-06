08b101d722248e998f1f8d3629f2f73e964426c8
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-06 11:15:16 +0200

    new

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import router from './routes/router.js';
 const app = express();
 app.use(express.json());                        // Configureer de app om JSON-gegevens te kunnen ontvangen en interpreteren
 app.use(express.urlencoded({extended: true}));  // Configureer de app om URL-gecodeerde gegevens te kunnen ontvangen en interpreteren
-app.use(express.static("public"));              // Configureer de app om statische bestanden te kunnen serveren vanuit de 'public' map
+let options = { maxAge: '2y' }                  // The cache data leeftijd before auto deleting
+app.use(express.static("public",options));      // Configureer de app om statische bestanden te kunnen serveren vanuit de 'public' map
 app.set('views', 'views')                       // Configureer de 'views' map als de directory voor alle weergavebestanden (views)
 app.set('view engine', 'ejs');                  // Configureer de 'ejs' module als de view engine voor de app
 
